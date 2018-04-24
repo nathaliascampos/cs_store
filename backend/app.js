c8892a7e910483d938fc2b126db0f9af9b7baedd
natycampos <nathaliasouza@gec.inatel.br>
2018-04-24 17:30:35 -0300

    apis

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,137 @@
-var restify = require('restify');
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
 
-function helloF(res, res, next){
-    var mensagem = {
-        texto: 'Hello world'
-    }
-
-    res.json(mensagem);
-    next();
+//Configuração do BD
+var objConn = {
+    host: 'localhost',
+    user: 'root'
+    password: '',
+    database: 'skin_cs'
 }
 
-var server = restify.createServer({
-    name: 'Node 1'
+var port = process.env.PORT || 5000; 
+
+//      API REST PELO EXPRESS:
+
+var user = express.Router();
+
+//INSERIR USER
+user.post('inserir', function(req, res)){
+    var login = req.body.login;
+    var senha = req.body.senha;
+
+    //var connection = mysql.createConnection(objConn)
+
+    var strQuery = "INSERT INTO user (login, senha) VALUES ('" + login + "', '" + senha + "');";
+
+    console.log(strQuery);
+
+    connection.query(strQuery, function(err, rows, fields) {
+        if (!err) {
+            res.jsonp(rows);
+        } else {
+            res.jsonp(err);
+        }
+    });
+
+    connection.end();
+}
+
+//ATUALIZAR USER
+user.post('/atualizar', function(req, res) {
+    var id = req.body.id;
+    var login = req.body.login;
+    var senha = req.body.senha;
+
+    var connection = mysql.createConnection(objConn);
+
+    connection.connect();
+
+    var strQuery = "UPDATE user SET login = '" + login + "', senha = '" + senha + "' WHERE id = " + id + ";";
+
+    console.log(strQuery);
+
+    connection.query(strQuery, function(err, rows, fields) {
+        if (!err) {
+            res.jsonp(rows);
+        } else {
+            res.jsonp(err);
+        }
+    });
+
+    connection.end();
 });
 
-// http://localhost:5000/hello -> chama função helloF GET NO POSTMAN
-server.get('/hello', helloF);
+//LISTAR TODOS USERS
+user.get('/listarTodos', function(req, res) {
+    var connection = mysql.createConnection(objConn);
+    connection.connect();
 
-server.listen(5000, function(){
-    console.log('%s sendo executado!', server.name)
+    var strQuery = "SELECT id, login, senha FROM user";
+
+    console.log(strQuery);
+
+    connection.query(strQuery, function(err, rows, fields) {
+        if (!err) {
+        	res.jsonp(rows);
+        } else {
+        	res.jsonp(err);
+        }
+    });
+
+    connection.end();
 });
+
+//BUSCAR USER POR ID:
+user.get('/buscarPorId/:id', function(req, res) {
+    var id = req.params.id; //Aqui pegamos o ID como parâmetro, pois método GET não tem Body
+
+    var connection = mysql.createConnection(objConn);
+
+    connection.connect();
+
+    var strQuery = "SELECT id, login, senha FROM user WHERE id = " + id;
+
+    console.log(strQuery);
+
+    connection.query(strQuery, function(err, rows, fields) {
+        if (!err) {
+            res.jsonp(rows);
+        } else {
+            res.jsonp(err);
+        }
+    });
+
+    connection.end();
+})
+
+//REMOVER USER:
+user.post('/remover', function(req, res) {
+    var id = req.body.id;
+
+    var connection = mysql.createConnection(objConn);
+
+    connection.connect();
+
+    var strQuery = "DELETE FROM user WHERE id = " + id;
+
+    console.log(strQuery);
+
+    connection.query(strQuery, function(err, rows, fields) {
+        if (!err) {
+            res.jsonp(rows);
+        } else {
+            res.jsonp(err);
+        }
+    });
+
+    connection.end();
+});
+
+app.use('/user', user);
+
+
+
+
+
